6ba6ef50a751f8e5b91c39dad7ae7e9355140be5
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-05-28 14:57:30 -0500

    feat: :sparkles: Cálculo del valor de un capital futuro

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
-// Reglamento de evaluaciones
 
-let nota1 = parseFloat(prompt ("What was your first score ?"));
-let nota2 = parseFloat(prompt ("What was your second score ?"));
+// Cálculo del valor de un capital futuro
 
-if (nota1 < 2 && nota2 < 2)  {
-    alert ("You failed");
+let capital = parseFloat(prompt ("What is your capital [$] ?"));
+let tiempo = parseFloat(prompt ("How long time is your period [years] ?"));
+let interes = parseFloat(prompt ("What is your interest rate [%] ?"));
+
+if (capital < 0) {
+    alert("El valor numerico del capital debe ser mayor o igual a 0");
 }
-else if (nota1 > 9 && nota2 > 9) {
-    alert ("You passed");
+else if (tiempo <= 0) {
+    alert("El valor numerico del tiempo debe ser menor a 0");
 }
-else {
-    let nota3 = parseFloat(prompt ("What was your third score ?"));
-    if ((nota1 + nota2 + nota3) / 3 < 3) {
-        alert ("You failed");
-    }
-    else if ((nota1 + nota2 + nota3) / 3 >= 7 ) {
-        alert ("You passed");
-    }
-    else {
-        let exam = parseFloat(prompt ("What was your exam score ?"));
-        if (exam < 5) {
-            alert ("You failed");
-        }
-    }
+else if (interes < 0 && interes > 100) {
+    alert("El valor numerico de la tasa de interes debe ser mayor a 0 y menor a 100");
 }
+
+let final = capital*((1+(interes/100))**tiempo);
+let capitalFinal = final.toLocaleString('es-ES', { minimumFractionDigits: 0, maximumFractionDigits: 0 });
+
+alert("Capital final es de: $"+ capitalFinal +" pesos");
