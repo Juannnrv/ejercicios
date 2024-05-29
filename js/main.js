05bb49d7e588902466250c097c4fa2912e70e662
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-05-29 16:19:25 -0500

    feat: :sparkles: Sumar rango de números

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-// Contar rango de números
+// Sumar rango de números
 
 let n1 = parseFloat(prompt("Dame el primer número "));
 let n2 = parseFloat(prompt("Dame el segundo número "));
 
-const contarRango = (n1, n2) => {
+const sumarRango = (n1, n2) => {
+
     let contador = 0;
 
-    for (let i = n1 + 1; i < n2; i++) {
-        contador++;
+    for (let i = n1 ; i <= n2; i++) {
+        contador += i;
     }
-
     return contador;
 }
 
-let res = contarRango(n1, n2);
+let res = sumarRango(n1, n2);
 alert(res);
