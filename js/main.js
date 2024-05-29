cc8aca18b255529905fe619130cdec05b2342a58
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-05-29 07:42:39 -0500

    feat: :sparkles: Contraseña válida

--- js/main.js
+++ js/main.js
@@ -1,35 +1,17 @@
+// Contraseña válida
 
-// Búsqueda de subsecuencia en ADN
-
-function esta_en_adn() {
-    const subsecuencia = prompt("Ingrese la subsecuencia de ADN:");
-
-    if (!subsecuencia || !/^[actg]+$/i.test(subsecuencia)) {
-        alert("La subsecuencia ingresada no es válida.");
-        return;
+const contraseñaValida = () => {
+    let contraseña = prompt("Dame la contraseña");
+    
+    if (contraseña === "2Fj(jjbFsuj" || contraseña === "eoZiugBf&g9") {
+        alert(true)
     }
-
-    const secuencia = "gtgggggggtttatgcctttagaacagcagactactgataactccaatcctgggttgaaaatgccaagggcgccagagagccaaacgatgagcgttggaccacaaacgataaaaactcac" +
-        "tttctccgtggggtgaaagcgattctttctggcccgtatccgccagcacttaaagttgcat" +
-        "tcggcgcggccctaccgctgctaattggggtaattgtcctaggattgtacgtaacgctt" +
-        "ggcgggcacagccgcaagaaagcccacgcagccgcgatagatgctttggtcgagaagcac" +
-        "gaagcatgctacaaggtccaagcaaagattgcacacggcaggcttgccttacagtccgct" +
-        "gtggtgtctgttgcggatgccagcatgcaacaactccagttcgtgcagcaaggaattctc" +
-        "atgtgtgtcggagagctcgacgatatgcagaagttccggacccgactggataatgaaatcagtgccatcaaccagcgaattcccagcattgtcgaggaggtaagaaaacacaccgacgat" +
-        "gcgcttgagtggaatcttgctagaaccaagaacattttagagggcactgaagagcgcctg" +
-        "aaggatatgggcaatgagttggtgcgctacctagacgatgctcgcgccctcattgaaaat" +
-        "gcacgtatagctgcaggatcaatgcaacacctcgttggtgatgaggtgagaaagcagctt" +
-        "gctgaggttctagtaaaagttgcagaagtaagtaatggctttattgcgcttaagaagagt" +
-        "gtatctggctatttggaaaaaagcagtggacttgttgctagggaagttagggcaatcctg" +
-        "gatgaccgcatgcgaagcctgcggaccatgtacaaaatgtgggatgcagaacaaaactcc" +
-        "gtagtcagcgtgtgtaccacgctccaaaaggcaagcatggaggctgccgcggtagcaagt";
-
-    const secuencia_minusculas = secuencia.toLowerCase();
-    const subsecuencia_minusculas = subsecuencia.toLowerCase();
-
-    if (secuencia_minusculas.includes(subsecuencia_minusculas)) {
-        alert("La subsecuencia está presente en la secuencia de ADN.");
-    } else {
-        alert("La subsecuencia no está presente en la secuencia de ADN.");
+    else {
+        alert(false)
     }
 }
+
+console.log(contraseñaValida("2Fj(jjbFsuj"))
+console.log(contraseñaValida("eoZiugBf&g9"))
+console.log(contraseñaValida("hola")) 
+console.log(contraseñaValida("")) 
