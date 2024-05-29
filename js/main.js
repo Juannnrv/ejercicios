33968b00bfb88ee4c97b910a9034bcda0c3ca185
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-05-29 15:43:51 -0500

    feat: :sparkles: FizzBuzz

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
-// Número de likes
+// FizzBuzz
 
-let num = parseFloat(prompt("Dame el número"))
+let num = parseFloat(prompt("Dame el número "))
 
-const likes = (num) => {
+const fizzBuzz = (num) => {
 
-    if (num < 1000) {
-        return num.toString();
-    } 
-    else if (num < 1000000) {
-        let k = Math.floor(num / 1000);
-        return k.toString() + "K";
-    } 
-    else {
-        let m = Math.floor(num / 1000000);
-        return m.toString() + "M";
+    if (num % 3 === 0 && num % 5 === 0) {
+        return "fizzbuzz";
+    }
+    else if (num % 5 === 0) {
+        return "buzz";
+    }
+    else if (num % 3 === 0) {
+        return "fizz" ;
     }
 }
 
-let res = likes(num);
+let res = fizzBuzz(num);
 alert(res)
