e981b7ff4c438e30e6454cb7eced196b86763743
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-05-29 19:52:10 -0500

    feat: :sparkles: Sumar arreglo entre el rango

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
-// Remover ceros
+// Sumar arreglo entre el rango
 
-const removerCeros = () => {
-
+const sumarArreglo = () => {
+    
     let elementos = [];
     let continuar = true;   
-
+    
     while (continuar) {
         let elemento = prompt("Ingresa un elemento del arreglo, si deseas no agregar más dejalo vacío:");
-
+        
         if (elemento === null || elemento === "") {
             continuar = false;
         } else {
             elementos.push(parseFloat(elemento));
         }
     }
+    
+    let n1 = parseFloat(prompt("Dame la posición inicial "));
+    let n2 = parseFloat(prompt("Dame la posición final  "));
 
-    let elementoAeliminar = 0
-    let nuevoarray = elementos.filter(item => item !== elementoAeliminar);
+    if (n1 < 0 || n2 >= elementos.length || n1 > n2) {
+        return "Lo siento, la posición que ingresaste no está dentro del rango del arreglo.";
+    }
 
-    return nuevoarray
+    let contador = 0;
+
+    for (let i = n1; i <= n2; i++) {
+        contador += elementos[i];   
+    }
+    return contador;
 }
 
-let res = removerCeros();
-alert(res.join(", "));
+let res = sumarArreglo();
+alert(res);
+
