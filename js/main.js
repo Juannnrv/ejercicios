8653fb8ba36e6fd5ea0683ca6e095b5edf9e4b80
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-05-29 20:41:59 -0500

    feat: :sparkles: Transcribir ADN a ARN

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
-// Sumar arreglo entre el rango
+// Transcribir ADN a ARN
 
-const sumarArreglo = () => {
-    
-    let elementos = [];
-    let continuar = true;   
-    
-    while (continuar) {
-        let elemento = prompt("Ingresa un elemento del arreglo, si deseas no agregar más dejalo vacío:");
-        
-        if (elemento === null || elemento === "") {
-            continuar = false;
-        } else {
-            elementos.push(parseFloat(elemento));
+let cadenaADN = prompt("Por favor, ingrese la cadena de ADN:");
+
+const transcribir = (cadenaADN) => {
+    const mapa = {
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U"
+    };
+
+    let cadenaARN = "";
+
+    for (let i = 0; i < cadenaADN.length; i++) {
+        let nucleotidoADN = cadenaADN[i];
+        let nucleotidoARN = mapa[nucleotidoADN];
+
+        if (!nucleotidoARN) {
+            return "Error: Cadena de ADN contiene nucleótidos no válidos.";
         }
-    }
-    
-    let n1 = parseFloat(prompt("Dame la posición inicial "));
-    let n2 = parseFloat(prompt("Dame la posición final  "));
 
-    if (n1 < 0 || n2 >= elementos.length || n1 > n2) {
-        return "Lo siento, la posición que ingresaste no está dentro del rango del arreglo.";
+        cadenaARN += nucleotidoARN;
     }
 
-    let contador = 0;
-
-    for (let i = n1; i <= n2; i++) {
-        contador += elementos[i];   
-    }
-    return contador;
+    return cadenaARN;
 }
 
-let res = sumarArreglo();
+
+let res = transcribir(cadenaADN);
 alert(res);
+
+
 
