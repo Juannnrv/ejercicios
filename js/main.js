6f7681e98620752d5f10eb63bbdc14f2a2d633db
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-01 20:56:22 -0500

    feat: :sparkles: Traducir números a palabras

--- js/main.js
+++ js/main.js
@@ -1,18 +1,49 @@
-// Imprimir una matriz
+// Traducir números a palabras
 
-const imprimirMatriz = () => {
+const numerosAPalabras = () => {
 
-    let matriz = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ];
+    let array = [5, 6, 7, 8, 9];
+    let nuevo = [];
 
-    matriz.forEach(fila => {
-        fila.forEach(elemento => {
-            console.log(elemento);
-        });
-    });
+    for (let i = 0; i < array.length; i++) {
+
+        num = array[i].toString();
+
+        switch(num) {
+            case "0":
+                nuevo.push("cero");
+                break;
+            case "1":
+                nuevo.push("uno");
+                break;
+            case "2":
+                nuevo.push("dos");
+                break;
+            case "3":
+                nuevo.push("tres");
+                break;
+            case "4":
+                nuevo.push("cuatro");
+                break;
+            case "5":
+                nuevo.push("cinco");
+                break;
+            case "6":
+                nuevo.push("seis");
+                break;
+            case "7":
+                nuevo.push("siete");
+                break;
+            case "8":
+                nuevo.push("ocho");
+                break;
+            case "9":
+                nuevo.push("nueve");
+                break;
+        }
+    }
+    console.log([nuevo.join(", ")]);
 }
 
-imprimirMatriz()
+numerosAPalabras();
+
